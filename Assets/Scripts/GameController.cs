710162ca7c98639e14138eb745ec3d1613025ccc
James Smith <james_smith.games@live.co.uk>
2017-03-17 15:48:27 +0000

    Basic chain setup made

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    static bool inputEnabled = true;
+    public static bool InputEnabled {
+        get { return inputEnabled; }
+        set { inputEnabled = value; }
+    }
+
+	public static void ChangeScene(string scene) {
+        SceneManager.LoadScene(scene);
+    }
+	
+    public static void ExitGame() {
+        Application.Quit();
+    }
+}
