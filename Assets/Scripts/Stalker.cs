1522b00df2f31fb66ab7d53ba37f22708980134b
James Smith <james_smith.games@live.co.uk>
2017-03-19 11:48:19 +0000

    Angler fish added. New build.

--- Assets/Scripts/Stalker.cs
+++ Assets/Scripts/Stalker.cs
@@ -10,39 +10,60 @@ public class Stalker : MonoBehaviour {
     Vector3[] node;
     int index = 0;
     float chaseTimer = 0;
+    Light light;
 
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
-
-        node = new Vector3[path.transform.childCount];
-        for (int i = 0; i < path.transform.childCount; i++) {
-            node[i] = path.transform.GetChild(i).position;
+        light = GetComponentInChildren<Light>();
+        if (path != null) {
+            
+            node = new Vector3[path.transform.childCount];
+            for (int i = 0; i < path.transform.childCount; i++) {
+                node[i] = path.transform.GetChild(i).position;
+            }
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (GameController.lampOn) {
-            chaseTimer += Time.deltaTime;
-            agent.speed = 12;
-            agent.SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
-        }
-        else {
-            agent.speed = 10;
-            if (Vector3.Distance(transform.position, node[index]) < 2) {
-                index++;
+        if (path != null)
+            if (GameController.lampOn) {
+                chaseTimer += Time.deltaTime;
+                agent.speed = 12;
+                agent.SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
             }
-            if (index > path.transform.childCount - 1)
-                index = 0;
-            agent.SetDestination(node[index]);
+            else {
+                agent.speed = 10;
+                if (Vector3.Distance(transform.position, node[index]) < 2) {
+                    index++;
+                }
+                if (index > path.transform.childCount - 1)
+                    index = 0;
+                agent.SetDestination(node[index]);
 
-        }
+            }
 	}
+
+    IEnumerator eat() {
+        light.enabled = true;
+
+        yield return new WaitForSeconds(0.25f);
+
+        GameController.Respawn();
+        GetComponent<Animator>().SetBool("Eat", false);
+        light.enabled = false;
+        yield return null;
+    }
 
     void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Player") {
-            GameController.Respawn();
+
+            if (path == null) {
+                GetComponent<Animator>().SetBool("Eat", true);
+                StartCoroutine("eat");
+            }
+            else
+                GameController.Respawn();
         }
     }
 }
